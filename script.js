b693c2b4a29d097edc87c8970ee9f811add63fd9
Rodrigo Henrique Fonseca Estrela <rohenri0808@gmail.com>
2024-04-24 15:44:57 -0300

    script: criando o programa para obter a hora e para tocar um som em uma hora específica

---
+++ script.js
@@ -0,0 +1,24 @@
+const horas = document.querySelector('#horas')
+const minutos = document.querySelector("#minutos");
+const segundos = document.querySelector("#segundos");
+
+const relogio = setInterval(function time() {
+    let datetoday = new Date()
+    let hr = datetoday.getHours();
+    let min = datetoday.getMinutes();
+    let seg = datetoday.getSeconds();
+
+    if (horas > 10) hr = '0' + hr;
+
+    document.querySelector("#horas").innerHTML = hr;
+    document.querySelector("#minutos").innerHTML = min;
+    document.querySelector("#segundos").innerHTML = seg;
+
+    const audio = document.querySelector('#meuAudio')
+    if (hr === 15 && min === 42) {
+        audio.play()
+    }
+    if (hr === 15 && min === 43) {
+        audio.pause
+    }
+})
